48538082898b0c5316a79eb3875b5b69e509ed50
sungho <sungho@localhost>
2020-07-22 16:48:13 +0900

    practice04 1번 다형성 및 오버로드 문제 구현

--- practice4/src/main/java/prob01/Printer.java
+++ practice4/src/main/java/prob01/Printer.java
@@ -1,5 +1,25 @@
 package prob01;
 
 public class Printer {
+//	private int num1;
+//	private boolean b;
+//	private float num2;
+//	private String name;
+
+	public void println(int i) {
+		System.out.println(i);
+	}
+
+	public void println(boolean c) {
+		System.out.println(c);
+	}
+
+	public void println(double d) {
+		System.out.println(d);
+	}
+
+	public void println(String string) {
+		System.out.println(string);
+	}
 
 }
