33e67539a6f921ff2ad0cda71f2e75ea1a735e1d
sungho <sungho@localhost>
2020-07-22 11:56:01 +0900

    Point를 상속받은 ColorPoint

--- chapter03/src/main/java/com/bit2020/chapter03/paint/MainApp.java
+++ chapter03/src/main/java/com/bit2020/chapter03/paint/MainApp.java
@@ -15,7 +15,15 @@ public class MainApp {
 		drawPoint(point1);
 		drawPoint(point2);
 		point1.show(false);
-
+		
+		System.out.println("=======================");
+		
+		Point point3 = new ColorPoint(); //업캐스팅
+		point3.setX(30);
+		point3.setY(100);
+		((ColorPoint)point3).setColor("red"); //부모에서 자식접근할때는 명시적으로 해줘야함. 
+		
+		drawPoint(point3);
 	}
 	
 	public static void drawPoint(Point pt) {
--- chapter03/src/main/java/com/bit2020/chapter03/tv/WatchTv.java
+++ chapter03/src/main/java/com/bit2020/chapter03/tv/WatchTv.java
@@ -3,27 +3,27 @@ package com.bit2020.chapter03.tv;
 public class WatchTv {
 
     public static void main( String[] args ) {
-        Tv tv = new Tv( 7, 20, false);  	
-         
-         tv.status();	
-         
-         tv.power( true );
-         tv.volume( 120 );
-         tv.status();		          
-         
-         tv.volume( false );
-         tv.status();
-         
-         tv.channel( 0 );
-         tv.status();		          
-         
-         tv.channel( true );
-         tv.channel( true );
-         tv.channel( true );
-         tv.status();
-
-         tv.power( false );
-         tv.status();      		          
+//        Tv tv = new Tv( 7, 20, false);  	
+//         
+//         tv.status();	
+//         
+//         tv.power( true );
+//         tv.volume( 120 );
+//         tv.status();		          
+//         
+//         tv.volume( false );
+//         tv.status();
+//         
+//         tv.channel( 0 );
+//         tv.status();		          
+//         
+//         tv.channel( true );
+//         tv.channel( true );
+//         tv.channel( true );
+//         tv.status();
+//
+//         tv.power( false );
+//         tv.status();      		          
      }
 
 }
