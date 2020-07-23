e30d9d7856f3ca1028dd7f597947ab449e65136d
KANGSUNGHO <kangsho15@gc.gachon.ac.kr>
2020-07-23 11:24:37 +0900

    못올라간 Book, Student commit

--- chapter03/src/main/java/com/bit2020/chapter03/person/TestStudent.java
+++ chapter03/src/main/java/com/bit2020/chapter03/person/TestStudent.java
@@ -3,7 +3,10 @@ package com.bit2020.chapter03.person;
 public class TestStudent {
 
 	public static void main(String[] args) {
-		Person p = new Student();
+		Student s = new Student();
+		
+		Person p = s; // 다운캐스팅, implicity(암시적)
+		Student s2 = (Student)p; // 업캐스팅, explicity(명시적)
 
 	}
 
