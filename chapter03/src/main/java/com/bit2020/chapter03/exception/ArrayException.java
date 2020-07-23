d6ba03fdcf0fffa1519d0f67bd10a929e860392d
KANGSUNGHO <kangsho15@gc.gachon.ac.kr>
2020-07-23 14:08:42 +0900

    java exception 처리 추가

---
+++ chapter03/src/main/java/com/bit2020/chapter03/exception/ArrayException.java
@@ -0,0 +1,14 @@
+package com.bit2020.chapter03.exception;
+
+public class ArrayException {
+
+	public static void main(String[] args) {
+		int[] a = { 10, 20, 30, 40 };
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.println(a[i]);
+		}
+
+	}
+
+}
