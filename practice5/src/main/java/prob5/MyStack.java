dc11bca81622ee9d0b2c5e0f5b857b26e8504bca
KANGSUNGHO <kangsho15@gc.gachon.ac.kr>
2020-07-23 17:31:51 +0900

    MyStack 구현 문제(Practice5-5)

--- practice5/src/main/java/prob5/MyStack.java
+++ practice5/src/main/java/prob5/MyStack.java
@@ -3,4 +3,35 @@ package prob5;
 public class MyStack {
 	private int top;
 	private String[] buffer;
+	private int count;
+	public MyStack() {
+		top = 0;
+		count = 0;
+	}
+	public MyStack(int i) {
+		buffer = new String[i];
+	}
+
+	public void push(String s) {
+		if( count < buffer.length) {
+			buffer[top++] = s;
+		}
+		count++;
+	}
+
+	public String pop() throws MyStackException {
+//		buffer[-top];
+		if (top == 0) {
+			throw new MyStackException();
+		} else {
+			return buffer[--top];
+		}
+	}
+
+	public boolean isEmpty() {
+		if (top == 0)
+				return true;
+		else
+			return false;
+	}
 }
