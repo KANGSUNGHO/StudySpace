7c9fa549f1d6b54b7a29e4f7a1ceabf96cfee6ea
KANGSUNGHO <kangsho15@gc.gachon.ac.kr>
2020-07-27 00:18:10 +0900

    Practice1 5번 문제 완료 - 1~99의 수애서 369에 짝 출력

--- practice01/src/main/java/practice01/Prob05.java
+++ practice01/src/main/java/practice01/Prob05.java
@@ -5,6 +5,19 @@ public class Prob05 {
 	public static void main(String[] args) {
 		
 		/* 코드를 작성합니다 */
+		// 369 반복문 테스트
+		for(int i = 1; i < 100; i++) {
+			
+			// 문자열로 형변환
+			String strCnt = String.valueOf(i); //String 으로 자료형 변환
+			// 형변환된 값의 길이 만큼 반복
+			for(int j = 0; j < strCnt.length(); j++) {
+				char chk = strCnt.charAt(j);
+				if(chk == '3' || chk == '6' | chk =='9') {
+					System.out.println(i + " 짝");
+				}
+			}
+		}
 	
 	}
 }
