718d61eec3c5947182f0b4bc8f88b2108fe896da
KANGSUNGHO <kangsho15@gc.gachon.ac.kr>
2020-07-28 15:10:55 +0900

    DAO 작성 및 테스트

---
+++ emaillist/src/main/java/com/bit2020/emaillist/dao/test/EmaillistDaoTest.java
@@ -0,0 +1,39 @@
+package com.bit2020.emaillist.dao.test;
+
+import java.util.List;
+
+import com.bit2020.emailist.dao.EmaillistDao;
+import com.bit2020.emaillist.vo.EamillistVo;
+
+public class EmaillistDaoTest {
+
+	public static void main(String[] args) {
+//		testInsert();
+		testSelectList();
+
+	}
+	public static void testSelectList() {
+		List<EamillistVo> list = new EmaillistDao().selectList();
+		if(list.size() == 2) {
+			System.out.println("성공");
+		} else {
+			System.out.println("실패");
+			
+		}
+	}
+	public static void testInsert() {
+		EamillistVo vo  = new EamillistVo();
+		vo.setFirstName("둘");
+		vo.setLastName("리");
+		vo.setEmail("dooly@gmail.com");
+		
+		boolean result = new EmaillistDao().insert(vo);
+		if(result) {
+			System.out.println("성공");
+		} else {
+			System.out.println("실패");
+		}
+		
+	}
+
+}
---
+++ emaillist/src/main/java/com/bit2020/emaillist/vo/EamillistVo.java
@@ -0,0 +1,57 @@
+package com.bit2020.emaillist.vo;
+
+public class EamillistVo {
+	private Long no;
+	private String firstName;
+	private String lastName;
+	private String email;
+	public Long getNo() {
+		return no;
+	}
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return super.clone();
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	@Override
+	protected void finalize() throws Throwable {
+		// TODO Auto-generated method stub
+		super.finalize();
+	}
+	public void setNo(Long no) {
+		this.no = no;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
