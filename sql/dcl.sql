efb90d561ca47b6b5eba4ef05b82d13c8d440cda
KANGSUNGHO <kangsho15@gc.gachon.ac.kr>
2020-07-28 17:40:55 +0900

    mysql 스키마 수정 및 DCL 수정

--- sql/dcl.sql
+++ sql/dcl.sql
@@ -1,8 +1,9 @@
 -- 1. DB 생성
 create database webdb;
-	-- drop database webdb;
+	-- drop database webdb; // 데이터베이스 삭제 
+	
 -- 2. DB user 생성 
-	-- drop user 'webdb'@'localhost'
+	-- drop user 'webdb'@'localhost' // user 삭제 
 create user 'webdb'@'localhost' identified by 'webdb';
 
 -- 3. grant privileges - webdb 라는 사용자에게 webdb의 권한을 줌
