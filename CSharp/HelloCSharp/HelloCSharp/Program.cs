1b5c2d4825c37fc2c9dc2385664763eb43e4e493
KANGSUNGHO <kangsho15@gc.gachon.ac.kr>
2020-11-18 21:41:01 +0900

    Hello C#

---
+++ CSharp/HelloCSharp/HelloCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(args.Length);
+            Console.ReadKey(); // 키를 입력할때까지 기다림.
+        }
+    }
+}
