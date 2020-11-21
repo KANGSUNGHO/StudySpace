28dd39f7c54ea67c793017cf46ff0e23670ab77f
KANGSUNGHO <kangsho15@gc.gachon.ac.kr>
2020-11-21 18:32:50 +0900

    C# calculator second

--- CSharp/HelloCSharpWin/HelloCSharpWin/Form1.cs
+++ CSharp/HelloCSharpWin/HelloCSharpWin/Form1.cs
@@ -24,8 +24,38 @@ namespace HelloCSharpWin
 
         private void SumNumbers_Click(object sender, EventArgs e)
         {
-            int number1 = Convert.ToInt32(Sum1.Text);
-            int number2 = Convert.ToInt32(Sum2.Text);
+            int number1 = 0;
+            int number2 = 0;
+
+            if (String.IsNullOrWhiteSpace(Sum1.Text))
+            {
+                MessageBox.Show("Sum1에 숫자를 입력해주세요.");
+                Sum1.Focus();
+                return; // function에서 빠져 나오고 싶을때 return해줌
+            }
+
+            if(int.TryParse(Sum1.Text, out number1) == false) // Sum1.Text가 숫자로 바꿀 수 있는 문자열이라면 number1에 숫자가 저장됨.
+            {
+                MessageBox.Show("Sum1에 문자가 들어왔습니다. 숫자를 입력해주세요.");
+                Sum1.SelectAll(); // Sum1에 있는 텍스트들을 다 선택해줌
+                Sum1.Focus();
+                return;
+            }
+
+            if (String.IsNullOrWhiteSpace(Sum2.Text))
+            {
+                MessageBox.Show("Sum2에 숫자를 입력해주세요.");
+                return; // function에서 빠져 나오고 싶을때 return해줌
+            }
+
+            if (int.TryParse(Sum2.Text, out number2) == false) // Sum2.Text가 숫자로 바꿀 수 있는 문자열이라면 number2에 숫자가 저장됨.
+            {
+                MessageBox.Show("Sum2에 문자가 들어왔습니다. 숫자를 입력해주세요.");
+                return;
+            }
+
+            number1 = Convert.ToInt32(Sum1.Text);
+            number2 = Convert.ToInt32(Sum2.Text);
 
             int sum = Add(number1, number2);
             SumResult.Text = sum.ToString();
