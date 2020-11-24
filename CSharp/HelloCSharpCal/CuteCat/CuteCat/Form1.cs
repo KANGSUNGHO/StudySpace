7b3d6260be09c270a2a7769aa7d6d99e8eab8e64
KANGSUNGHO <kangsho15@gc.gachon.ac.kr>
2020-11-24 21:54:42 +0900

    c# class, public, private, generator

---
+++ CSharp/HelloCSharpCal/CuteCat/CuteCat/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CuteCat
+{
+    public partial class Form1 : Form
+    {
+        private Cat MyCat = new Cat("sung",1);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Feed_Click(object sender, EventArgs e)
+        {
+            MyCat.Eat();
+            CatScreen.Text = MyCat.Express();
+        }
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+            MyCat.Play();
+            CatScreen.Text = MyCat.Express();
+        }
+
+        private void CatTimer_Tick(object sender, EventArgs e)
+        {
+            MyCat.GetBored();
+            CatScreen.Text = MyCat.Express();
+        }
+    }
+
+}
