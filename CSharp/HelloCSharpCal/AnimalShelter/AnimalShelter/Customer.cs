449b58f51e32b62cbdbef84078ab754a22ed5169
KANGSUNGHO <kangsho15@gc.gachon.ac.kr>
2020-11-26 02:23:20 +0900

    c# class attribute, DateTime

---
+++ CSharp/HelloCSharpCal/AnimalShelter/AnimalShelter/Customer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelter
+{
+    public class Customer
+    {
+        public string FirstName;
+        public string LastName;
+        private DateTime BirthDay;
+        private bool IsQualified;
+        public string Address;
+        public string Description;
+
+        public Customer(string firstName, string lastName, DateTime BirthDay)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.BirthDay = BirthDay;
+
+            this.IsQualified = Age1 >= 18;
+        }
+
+        /*
+        public int getAge()
+        {
+            return Age;
+        }
+
+        public void setAge(int Age)
+        {
+            this.Age = Age;
+            IsQualified = Age >= 18;
+        }
+        */
+        public DateTime BirthDay1
+        {
+            get
+            {
+                return BirthDay;
+            }
+            set
+            {
+                BirthDay = value;
+                IsQualified = Age1 >= 18;
+            }
+        }
+
+        public int Age1
+        {
+            get {
+                return DateTime.Now.Year - BirthDay.Year;
+            }
+        }
+
+        
+        /* 
+        public bool getIsQualified()
+        {
+            return IsQualified;
+        }
+        */
+
+        public bool IsQualified1
+        {
+            get
+            {
+                return IsQualified;
+            }
+        }
+        /*
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+        */
+
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+    }
+}
---
+++ CSharp/HelloCSharpCal/AnimalShelter/AnimalShelter/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnimalShelter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateCustomer_Click(object sender, EventArgs e)
+        {
+
+            Customer cus = new AnimalShelter.Customer("Ian", "Na", new DateTime(1996,1,1));
+            cus.Address = "경기도 의정부";
+            cus.Description = "안녕하세요 반갑습니다.";
+
+            CusFullName.Text = cus.FullName;
+            CusAddress.Text = cus.Address;
+            CusAge.Text = cus.Age1.ToString();
+            CusDescription.Text = cus.Description;
+            CusIsQualified.Text = cus.IsQualified1.ToString();
+        }
+    }
+}
